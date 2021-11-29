72c6cecb80dfab72ae8806e08e06d3bc2b3275ff
qwzz <89381182+qwyzex@users.noreply.github.com>
2021-11-29 11:31:49 +0700

    v1

---
+++ reset.sh
@@ -0,0 +1,18 @@
+yellow='\033[1;33m'
+blue='\033[0;34m'
+nc='\033[0m'
+
+echo -e "${yellow}WARNING!!!${nc} THIS WILL RESET ${blue}'.bashrc'${nc} AND ${blue}'.bash_profile'${nc} TO IT'S DEFAULT STATE! BE CAREFULL!"
+select yn in "Yes" "No"; do
+	case $yn in 
+		Yes ) cp .bash_profile ~; rm ~/.bashrc; rm -r ~/qxBash; echo "FINISHED"; break;;
+		No ) echo "CANCELLED"; exit;;
+	esac
+done
+echo -e "\nDO YOU ALSO WANT TO DELETE '.bashrc' FILE FROM THE HOME DIRECTORY"
+select yn in "Yes" "No"; do
+	case $yn in 
+		Yes ) cp .bash_profile ~; rm ~/.bashrc; rm -r ~/qxBash; echo "FINISHED"; break;;
+		No ) echo "CANCELLED"; exit;;
+	esac
+done
