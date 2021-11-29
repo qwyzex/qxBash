8d75d1da50eeb9eadf4df03f6b0f32f61f9cc9de
qwyzex <qwyzex@gmail.com>
2021-11-29 11:55:23 +0700

    Fix Reset Script

--- reset.sh
+++ reset.sh
@@ -5,14 +5,14 @@ nc='\033[0m'
 echo -e "${yellow}WARNING!!!${nc} THIS WILL RESET ${blue}'.bashrc'${nc} AND ${blue}'.bash_profile'${nc} TO IT'S DEFAULT STATE! BE CAREFULL!"
 select yn in "Yes" "No"; do
 	case $yn in 
-		Yes ) cp .bash_profile ~; rm ~/.bashrc; rm -r ~/qxBash; echo "FINISHED"; break;;
+		Yes ) cp .bash_profile ~; rm ~/.bashrc; rm -r ~/qxBash; echo "FINISHED RESETING '.bash_profile' AND DELETING 'qxBash' FOLDER IN HOME DIR"; break;;
 		No ) echo "CANCELLED"; exit;;
 	esac
 done
 echo -e "\nDO YOU ALSO WANT TO DELETE '.bashrc' FILE FROM THE HOME DIRECTORY"
 select yn in "Yes" "No"; do
 	case $yn in 
-		Yes ) cp .bash_profile ~; rm ~/.bashrc; rm -r ~/qxBash; echo "FINISHED"; break;;
+		Yes ) rm ~/.bashrc; echo "FINISHED DELETING '.bashrc'"; break;;
 		No ) echo "CANCELLED"; exit;;
 	esac
 done
