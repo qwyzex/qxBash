abfc8d2338bd3d7f455b1c025bc7fcb9edceb08a
qwyzex <qwyzex@gmail.com>
2021-11-29 12:01:09 +0700

    Fix double rm '.bashrc'

--- reset.sh
+++ reset.sh
@@ -5,7 +5,7 @@ nc='\033[0m'
 echo -e "${yellow}WARNING!!!${nc} THIS WILL RESET ${blue}'.bashrc'${nc} AND ${blue}'.bash_profile'${nc} TO IT'S DEFAULT STATE! BE CAREFULL!"
 select yn in "Yes" "No"; do
 	case $yn in 
-		Yes ) cp .bash_profile ~; rm ~/.bashrc; rm -r ~/qxBash; echo "FINISHED RESETING '.bash_profile' AND DELETING 'qxBash' FOLDER IN HOME DIR"; break;;
+		Yes ) cp .bash_profile ~; rm -r ~/qxBash; echo "FINISHED RESETING '.bash_profile' AND DELETING 'qxBash' FOLDER IN HOME DIR"; break;;
 		No ) echo "CANCELLED"; exit;;
 	esac
 done
